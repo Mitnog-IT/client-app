870a7da8cbde804a3d5b84066c8668b738076f8a
Shahadat Robin <sh.robin025@gmail.com>
2021-07-07 04:13:04 +0600

    service card fixed and logo added

--- components/content/NavBar.jsx
+++ components/content/NavBar.jsx
@@ -2,15 +2,17 @@ import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
 import style from '../../styles/NavBar.module.css';
 import Link from 'next/link';
 import { useEffect, useState } from "react";
+import Image from "next/image";
+import logo from '../../public/logo1-removebg-preview.png';
 
 const NavBar = () => {
-    const [navBarColor, setNavBarColor] = useState(false);
+    const [isScroll, setIsScroll] = useState(false);
 
     const changeNavBar = () => {
         if (window.scrollY > 95) {
-            return setNavBarColor(true)
+            return setIsScroll(true)
         }
-        setNavBarColor(false)
+        setIsScroll(false)
     }
 
     useEffect(() => {
@@ -22,11 +24,13 @@ const NavBar = () => {
 
     return (
         <Navbar expand="lg" fixed="top"
-            className={navBarColor ? `${style.navBar}` : 'py-4'}
-            style={{ fontFamily: '"Roboto Slab", serif', transition: '.8s ease' }}>
+            className={isScroll ? `${style.scrollNavBar}` : 'py-3'}
+            style={{ transition: '0.8s ease', fontFamily: '"Roboto Slab", serif' }}>
             <Container>
                 <Link href="/">
-                    <a style={{ textDecoration: 'none', color: '#fff' }}><h5>Mitnog-IT</h5></a>
+                    <a>
+                        <Image src={logo} alt="mitnog it" width={200} height={66} />
+                    </a>
                 </Link>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
