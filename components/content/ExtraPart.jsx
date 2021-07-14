c485afd31c49f4cd507a49a342a4a2d25372ce88
Shahadat Robin <sh.robin025@gmail.com>
2021-07-14 08:20:33 +0600

    particle added done

--- components/content/ExtraPart.jsx
+++ components/content/ExtraPart.jsx
@@ -1,21 +1,89 @@
-
+import Particles from "react-tsparticles";
 
 const ExtraPart = () => {
     return (
-        <section className="container d-flex justify-content-around align-items-center"
+        <section
             style={{
                 minHeight: '150px',
                 color: '#0aa2c0',
                 textAlign: 'center',
-                fontFamily: '"Roboto Slab", serif'
+                fontFamily: '"Roboto Slab", serif',
+                position: 'relative',
+                overflow: "hidden"
             }}>
-            <h3>Are you ready to get our service ?</h3>
-            <button style={{
-                padding: '7px 50px',
-                border: 'none',
-                background: '#0aa2c0',
-                color: '#fff'
-            }}>Yes</button>
+            <div style={{ position: 'absolute' }}>
+                <Particles height="150px" width="100vw"
+                    id="tsparticles"
+                    options={{
+                        fpsLimit: 100,
+                        interactivity: {
+                            detectsOn: "canvas",
+                            modes: {
+                                bubble: {
+                                    distance: 300,
+                                    duration: 2,
+                                    opacity: 0.8,
+                                    size: 40,
+                                },
+                                repulse: {
+                                    distance: 300,
+                                    duration: 0.9,
+                                },
+                            },
+                        },
+                        particles: {
+                            color: {
+                                value: "#0aa2c0",
+                            },
+                            links: {
+                                color: "#fff",
+                                distance: 1,
+                                enable: false,
+                                opacity: 0.9,
+                                width: .1,
+                            },
+                            collisions: {
+                                enable: true,
+                            },
+                            move: {
+                                direction: "top",
+                                enable: true,
+                                outMode: "bounce",
+                                random: true,
+                                speed: 3,
+                                straight: false,
+                            },
+                            number: {
+                                density: {
+                                    enable: true,
+                                    value_area: 600,
+                                },
+                                value: 130,
+                            },
+                            opacity: {
+                                value: 0.6,
+                            },
+                            shape: {
+                                type: "circle",
+                            },
+                            size: {
+                                random: true,
+                                value: 4,
+                            },
+                        },
+                        detectRetina: true,
+                    }} />
+            </div>
+            <div className="container d-flex justify-content-around align-items-center"
+                style={{ minHeight: '150px',position: 'relative' }}>
+                <h3>So Are you ready to get our service ?</h3>
+                <button style={{
+                    padding: '7px 50px',
+                    border: 'none',
+                    background: '#0aa2c0',
+                    color: '#fff'
+                }}>Yes</button>
+            </div>
         </section>
     );
 };
