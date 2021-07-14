7705a599ce655c48e8d60a3650833f616ec23edd
Shahadat Robin <sh.robin025@gmail.com>
2021-07-15 03:21:09 +0600

    chat added

--- components/Layout.js
+++ components/Layout.js
@@ -1,11 +1,16 @@
 import style from '../styles/Layout.module.css'
 import NavBar from './content/NavBar';
+import MessengerCustomerChat from 'react-messenger-customer-chat';
 
 const Layout = ({ children }) => {
     return (
         <main className={style.container}>
             <NavBar />
             {children}
+            <MessengerCustomerChat
+                pageId="105753884733982"
+                appId="631074661202629"
+            />
         </main>
     );
 };
