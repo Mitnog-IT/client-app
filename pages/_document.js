0b4c5da36ab8ef124f18dd94f72f446b6e638f91
Shahadat Robin <sh.robin025@gmail.com>
2021-07-20 08:27:51 +0600

    meta updated

--- pages/_document.js
+++ pages/_document.js
@@ -7,19 +7,25 @@ class MyDocument extends Document {
             <Html lang="en">
                 <Head>
                     <link rel="icon" href="/favicon_2.ico" />
-                    <meta name="author" content="Mr.Hunter" />
-                    <meta name="description" content=" Xbox Gift Cards Freehere is a brand new website which will give you the opportunity to get Gift Cards. By having a Gift Card you will be given the opportunity to purchase games and other apps from online stores." />
-                    <meta name="keywords" content=" Xbox Gift Cards, Gift Card Codes, Get Gift Card Codes" />
-                    <meta property="og:description" content=" Xbox Gift Cards Freehere is a brand new website which will give you the opportunity to get Gift Cards. By having a Gift Card you will be given the opportunity to purchase games and other apps from online stores." />
+                    <meta name="author" content="Mitnog IT" />
+                    <meta name="description" content=" welcome to mitnogit. we are a information and communicaion services provider company. we can can give you digital marketing, seo, social media marketing, sem, smm, aso, web development, web design, web bug fix, email marketing, video marketing, logo design, business card design, softwere development, mobile development, ecommarce development, cms development, wordpress development, etc." />
+                    <meta name="keywords" content=" digital marketing, seo, social media marketing, sem, smm, aso, web development, web design, web bug fix, email marketing, video marketing, logo design, business card design, softwere development, mobile development, ecommarce development, cms development, wordpress development, etc" />
+                    <meta property="og:description" content=" welcome to mitnogit. we are a information and communicaion services provider company. we can can give you digital marketing, seo, social media marketing, sem, smm, aso, web development, web design, web bug fix, email marketing, video marketing, logo design, business card design, softwere development, mobile development, ecommarce development, cms development, wordpress development, etc." />
                     <meta property="og:image" content="img/fb.png" />
                     <meta property="og:locale" content="en_US" />
                     <meta property="og:type" content="website" />
-                    <meta property="og:site_name" content="Gift card Code 2021" />
-                    <meta property="og:title" content="Huge Collection Of Xbox Gift Cards" />
-                    <meta property="og:url" content="index-2.html" />
-                    <meta name="twitter:card" content="summary" />
-                    <meta name="twitter:description" content=" Xbox Gift Cards Freehere is a brand new website which will give you the opportunity to get Gift Cards. By having a Gift Card you will be given the opportunity to purchase games and other apps from online stores" />
-                    <meta name="twitter:title" content="Huge Collection of Xbox Gift Cards" />
+                    <meta property="og:site_name" content="Mitnog IT" />
+                    <meta property="og:title" content="Our Tech Your Future" />
+                    <meta property="og:url" content="https://mitnogit.com" />
+                    <meta name="twitter:og:url" content="https://twitter.com/mitnogit" />
+                    <meta name="twitter:description" content="  welcome to mitnogit. we are a information and communicaion services provider company. we can can give you digital marketing, seo, social media marketing, sem, smm, aso, web development, web design, web bug fix, email marketing, video marketing, logo design, business card design, softwere development, mobile development, ecommarce development, cms development, wordpress development, etc" />
+                    <meta name="twitter:title" content="Mitnog IT" />
+                    <meta name="facebook:og:url" content="https://faceook.com/mitnogit" />
+                    <meta name="facebook:description" content="  welcome to mitnogit. we are a information and communicaion services provider company. we can can give you digital marketing, seo, social media marketing, sem, smm, aso, web development, web design, web bug fix, email marketing, video marketing, logo design, business card design, softwere development, mobile development, ecommarce development, cms development, wordpress development, etc" />
+                    <meta name="facebook:title" content="Mitnog IT" />
+                    <meta name="linkedin:og:url" content="https://www.linkedin.com/company/mitnogit" />
+                    <meta name="linkedin:description" content="  welcome to mitnogit. we are a information and communicaion services provider company. we can can give you digital marketing, seo, social media marketing, sem, smm, aso, web development, web design, web bug fix, email marketing, video marketing, logo design, business card design, softwere development, mobile development, ecommarce development, cms development, wordpress development, etc" />
+                    <meta name="linkedin:title" content="Mitnog IT" />
 
                     <meta name="google-site-verification" content="u6jaZCPZ8UrzStm-8aYfl1kv1Lm7WWS67EQshmkkrXI" />
                     <meta name="yandex-site-verification" content="ab9f9cf3ee34e7be" />
